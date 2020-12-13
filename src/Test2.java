72c1a41b336be9292470ca562ff6383508cb1c46
swapnalij1 <swapnalij4050@gmail.com>
2020-12-13 12:50:14 +0530

    First commit

--- src/Test2.java
+++ src/Test2.java
@@ -3,7 +3,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testcase2");
+		System.out.println("HELLO");
+		System.out.println("tECTCASE1");
 
 	}
 
